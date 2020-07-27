652fd454fe873fdb041eff639ebb6c8443309111
hienclubvn <danghien719@gmail.com>
2020-07-28 00:00:51 +0700

    add script file (robot_fake)

---
+++ run_fake.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /opt/ros/melodic/setup.bash &
+source /opt/ros/melodic/setup.bash &
+
+roslaunch thesis_robot_fake thesis_robot_fake.launch
+
+#rosrun teleop_twist_keyboard teleop_twist_keyboard.py
